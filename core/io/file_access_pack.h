88631c46c0a5f0608afa9e5e8f9da126d5a0cf08
Mikhail Schmalzried <mikhail@photonable.com>
2026-07-16 21:46:19 -0600

    Revert "Add Steamworks auth & change pack magic"
    This reverts commit 3202c4a6134b6238757c1f71954d0fb4ce75b675.

--- core/io/file_access_pack.h
+++ core/io/file_access_pack.h
@@ -36,9 +36,8 @@
 #include "core/templates/hash_set.h"
 #include "core/templates/list.h"
 
-// Godot's packed file magic header
-// Little-endian hex string for 'PHTN'
-#define PACK_HEADER_MAGIC 0x5048544E
+// Godot's packed file magic header ("GDPC" in ASCII).
+#define PACK_HEADER_MAGIC 0x43504447
 
 #define PACK_FORMAT_VERSION_V2 2
 #define PACK_FORMAT_VERSION_V3 3
